e4646303a7072320586e09a7bf8b0f6a3e2b5e62
simplysabir <simplysabir@gmail.com>
2023-11-02 19:58:26 +0530

    api routes added

---
+++ app/api/quotes/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import getAllQuotes from "@/lib/getAllQuotes";
+
+export async function GET(request: Request) {
+    const quotes = await getAllQuotes();
+    
+    return NextResponse.json(quotes);
+}
---
+++ app/api/randomquote/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import getRandomQuote from "@/lib/getRandomQuote";
+
+export async function GET(request: Request) {
+    const quote = await getRandomQuote();
+    
+    return NextResponse.json(quote);
+}
